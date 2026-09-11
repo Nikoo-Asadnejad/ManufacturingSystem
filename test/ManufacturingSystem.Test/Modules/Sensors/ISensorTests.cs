1d8e11a9376038adf291d64ff2c5016d52d57361
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2026-09-11 09:58:41 +0200

    test(sensor): add sensor test

---
+++ test/ManufacturingSystem.Test/Modules/Sensors/ISensorTests.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using Broadcaster;
+using ManufacturingSystem.Modules.Sensors;
+using Moq;
+
+namespace ManufacturingSystem.Test.Modules.Sensors;
+
+public sealed class ISensorTests
+{
+    [Fact]
+    public async Task ReadAsync_Broadcasts()
+    {
+        // Arrange
+        var broadcaster = MockBroadcaster();
+
+        ISensor sensor = new TemperatureSensor(broadcaster.Object);
+
+        // Act
+        await sensor.ReadAsync(CancellationToken.None);
+
+        // Assert
+        Assert.Equal(SensorType.Temperature, sensor.SensorType);
+        broadcaster.Verify(b=>
+            b.BroadcastAsync(It.IsAny<IBroadcastEvent>() , 
+                It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    private static Mock<IBroadcaster> MockBroadcaster()
+    {
+        var broadcaster = new Mock<IBroadcaster>();
+        broadcaster
+            .Setup(configuredBroadcaster => configuredBroadcaster.BroadcastAsync(
+                It.IsAny<IBroadcastEvent>(),
+                CancellationToken.None));
+        return broadcaster;
+    }
+}
